29b3e3ee7f27e36a6d6f704863f1251df5646d4e
Kamylly <kamylylopes141@gmail.com>
2025-04-10 10:43:37 -0400

    Add files via upload

---
+++ correção.js
@@ -0,0 +1,28 @@
+//Exercicio 01:
+
+let nome = 'Kamylly';
+let idade = 15;
+console.log("Olá, meu nome é " + nome + " e eu tenho " + idade + " anos.");
+console.log(`Olá, meu nome é ${nome} e eu tenho ${idade} anos.`);
+
+
+//Exercicio 02:
+
+let texto = "Olá me nome é Suho";
+
+console.log(texto.toUpperCase());
+console.log(texto.toLowerCase());
+
+
+//Exercicio 03:
+
+let anos_de_carreira = 20;
+console.log(typeof anos_de_carreira);
+
+
+//Exercicio 04:
+
+let peso = 60;
+let altura = 1.66;
+let divisao_imc = (peso / (altura * altura)).toFixed(2);
+console.log(`Seu IMC é: ${imc}`);
